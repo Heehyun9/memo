48487f26ba680a284fc5d0210779e54ef1cf09d2
노희현 <81168262+Heehyun9@users.noreply.github.com>
2025-09-17 12:14:23 +0900

    IoC 컨테이너와 Bean

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,17 +4,33 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MemoService {
 
     private final MemoRepository memoRepository;
 
+//    public MemoService(ApplicationContext context){
+//        //1. 'Bean'이름으로 가져오기
+//        MemoRepository memoRepository = (MemoRepository)context.getBean("memoRepository");
+//
+//        //2. 'Bean' 클래스 형식으로 가져오기
+//        MemoRepository memoRepository = context.getBean(MemoRepository.class);
+//
+//        this.memoRepository = memoRepository;
+//    }
+
+    //Bean 주입 방법 4가지 (1번 추천)
+    //1. 생성자 오버로딩으로 주입
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
-
+    //2. 클래스 위에 @RequiredArgsConstructor로 주입
+    //3. MemoRepository 위에 @Autowired
+    //4. 메서드 주입도 가능 (위에 @Autowired)
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         // RequestDto -> Entity
